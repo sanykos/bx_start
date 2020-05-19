f3d71f609bf126025ca2e530e68abe6b76e1ebf5
sanykos <kosih2009@yandex.ru>
2020-05-19 13:51:48 +0300

    system auth form

---
+++ local/templates/agrolavka/components/bitrix/system.auth.form/default/template.php
@@ -0,0 +1,235 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+CJSCore::Init();
+?>
+
+<?
+if (strlen($_POST['ajax_key']) && $_POST['ajax_key']==md5('ajax_'.LICENSE_KEY)) {
+   $APPLICATION->RestartBuffer();
+   if (!defined('PUBLIC_AJAX_MODE')) {
+       define('PUBLIC_AJAX_MODE', true);
+   }
+   header('Content-type: application/json');
+   if ($arResult['ERROR']) {
+       echo json_encode(array(
+           'type' => 'error',
+           'message' => strip_tags($arResult['ERROR_MESSAGE']['MESSAGE']),
+       ));
+   } else {
+       echo json_encode(array('type' => 'ok'));
+   }
+   require($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/epilog_after.php');
+   die();
+}
+?>
+
+<div class="bx-system-auth-form">
+
+<?
+if ($arResult['SHOW_ERRORS'] == 'Y' && $arResult['ERROR'])
+	ShowMessage($arResult['ERROR_MESSAGE']);
+?>
+
+<?if($arResult["FORM_TYPE"] == "login"):?>
+
+<form class="auth-form" name="system_auth_form<?=$arResult["RND"]?>" method="post" target="_top" action="<?=$arResult["AUTH_URL"]?>">
+	<input type="hidden" name="ajax_key" value="<?= md5('ajax_'.LICENSE_KEY)?>" />
+<?if($arResult["BACKURL"] <> ''):?>
+	<input type="hidden" name="backurl" value="<?=$arResult["BACKURL"]?>" />
+<?endif?>
+<?foreach ($arResult["POST"] as $key => $value):?>
+	<input type="hidden" name="<?=$key?>" value="<?=$value?>" />
+<?endforeach?>
+	<input type="hidden" name="AUTH_FORM" value="Y" />
+	<input type="hidden" name="TYPE" value="AUTH" />
+	<table width="100%">
+		<tr>
+			<td colspan="2">
+				<div class="form-group">
+					<label for="auth_login_field" class="bx-soa-custom-label"><?=GetMessage("AUTH_LOGIN")?></label>
+					<input type="text" class="form__control" id="auth_login_field" name="USER_LOGIN" maxlength="50" value="" size="17" />
+				</div>
+			
+			<script>
+				BX.ready(function() {
+					var loginCookie = BX.getCookie("<?=CUtil::JSEscape($arResult["~LOGIN_COOKIE_NAME"])?>");
+					if (loginCookie)
+					{
+						var form = document.forms["system_auth_form<?=$arResult["RND"]?>"];
+						var loginInput = form.elements["USER_LOGIN"];
+						loginInput.value = loginCookie;
+					}
+				});
+			</script>
+			</td>
+		</tr>
+		<tr>
+			<td colspan="2">
+				<div class="form-group">
+					<label for="auth_passwd_field" class="bx-soa-custom-label"><?=GetMessage("AUTH_PASSWORD")?></label>
+					<input type="password" class="form__control" id="auth_passwd_field" name="USER_PASSWORD" maxlength="255" size="17" autocomplete="off" />
+				</div>
+			
+<?if($arResult["SECURE_AUTH"]):?>
+				<span class="bx-auth-secure" id="bx_auth_secure<?=$arResult["RND"]?>" title="<?echo GetMessage("AUTH_SECURE_NOTE")?>" style="display:none">
+					<div class="bx-auth-secure-icon"></div>
+				</span>
+				<noscript>
+				<span class="bx-auth-secure" title="<?echo GetMessage("AUTH_NONSECURE_NOTE")?>">
+					<div class="bx-auth-secure-icon bx-auth-secure-unlock"></div>
+				</span>
+				</noscript>
+<script type="text/javascript">
+document.getElementById('bx_auth_secure<?=$arResult["RND"]?>').style.display = 'inline-block';
+</script>
+<?endif?>
+			</td>
+		</tr>
+<?if ($arResult["STORE_PASSWORD"] == "Y"):?>
+		<tr class="user-remember__box">
+			<td valign="top"><input type="checkbox" id="USER_REMEMBER_frm" name="USER_REMEMBER" value="Y" /></td>
+			<td width="100%"><label for="USER_REMEMBER_frm" title="<?=GetMessage("AUTH_REMEMBER_ME")?>"><?echo GetMessage("AUTH_REMEMBER_SHORT")?></label></td>
+		</tr>
+<?endif?>
+<?if ($arResult["CAPTCHA_CODE"]):?>
+		<tr>
+			<td colspan="2">
+			<?echo GetMessage("AUTH_CAPTCHA_PROMT")?>:<br />
+			<input type="hidden" name="captcha_sid" value="<?echo $arResult["CAPTCHA_CODE"]?>" />
+			<img src="/bitrix/tools/captcha.php?captcha_sid=<?echo $arResult["CAPTCHA_CODE"]?>" width="180" height="40" alt="CAPTCHA" /><br /><br />
+			<input type="text" name="captcha_word" maxlength="50" value="" /></td>
+		</tr>
+<?endif?>
+
+		<tr>
+			<td colspan="2">
+				<div class="d-flex justify-content-end">
+					<button type="button" class="login-cancel-btn close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">Отменить</span>
+					</button>
+					<input type="submit" class="btn-login btn-round" name="Login" value="<?=GetMessage("AUTH_LOGIN_BUTTON")?>" />
+				</div>
+		</td>
+		</tr>
+		<tr>
+		<td colspan="2">
+			<div class="or-delimeter__box">
+				<div class="or-delimeter">
+					или
+				</div>
+			</div>
+	</td>
+</tr>
+<?if($arResult["NEW_USER_REGISTRATION"] == "Y"):?>
+		<tr>
+			<td colspan="2"><noindex><a href="<?=$arResult["AUTH_REGISTER_URL"]?>" class="modal-registration-btn btn-round" rel="nofollow"><?=GetMessage("AUTH_REGISTER")?></a></noindex><br /></td>
+		</tr>
+<?endif?>
+
+		<tr class="forgot-pswd__box">
+			<td colspan="2"><noindex><a href="<?=$arResult["AUTH_FORGOT_PASSWORD_URL"]?>" rel="nofollow"><?=GetMessage("AUTH_FORGOT_PASSWORD_2")?></a></noindex></td>
+		</tr>
+<?if($arResult["AUTH_SERVICES"]):?>
+		<tr>
+			<td colspan="2" style="display:none;">
+				<div class="bx-auth-lbl"><?=GetMessage("socserv_as_user_form")?></div>
+<?
+$APPLICATION->IncludeComponent("bitrix:socserv.auth.form", "icons", 
+	array(
+		"AUTH_SERVICES"=>$arResult["AUTH_SERVICES"],
+		"SUFFIX"=>"form",
+	), 
+	$component, 
+	array("HIDE_ICONS"=>"Y")
+);
+?>
+			</td>
+		</tr>
+<?endif?>
+	</table>
+</form>
+
+<?if($arResult["AUTH_SERVICES"]):?>
+<?
+$APPLICATION->IncludeComponent("bitrix:socserv.auth.form", "", 
+	array(
+		"AUTH_SERVICES"=>$arResult["AUTH_SERVICES"],
+		"AUTH_URL"=>$arResult["AUTH_URL"],
+		"POST"=>$arResult["POST"],
+		"POPUP"=>"Y",
+		"SUFFIX"=>"form",
+	), 
+	$component, 
+	array("HIDE_ICONS"=>"Y")
+);
+?>
+<?endif?>
+
+<?
+elseif($arResult["FORM_TYPE"] == "otp"):
+?>
+
+<form name="system_auth_form<?=$arResult["RND"]?>" method="post" target="_top" action="<?=$arResult["AUTH_URL"]?>">
+<?if($arResult["BACKURL"] <> ''):?>
+	<input type="hidden" name="backurl" value="<?=$arResult["BACKURL"]?>" />
+<?endif?>
+	<input type="hidden" name="AUTH_FORM" value="Y" />
+	<input type="hidden" name="TYPE" value="OTP" />
+	<table width="95%">
+		<tr>
+			<td colspan="2">
+			<?echo GetMessage("auth_form_comp_otp")?><br />
+			<input type="text" name="USER_OTP" maxlength="50" value="" size="17" autocomplete="off" /></td>
+		</tr>
+<?if ($arResult["CAPTCHA_CODE"]):?>
+		<tr>
+			<td colspan="2">
+			<?echo GetMessage("AUTH_CAPTCHA_PROMT")?>:<br />
+			<input type="hidden" name="captcha_sid" value="<?echo $arResult["CAPTCHA_CODE"]?>" />
+			<img src="/bitrix/tools/captcha.php?captcha_sid=<?echo $arResult["CAPTCHA_CODE"]?>" width="180" height="40" alt="CAPTCHA" /><br /><br />
+			<input type="text" name="captcha_word" maxlength="50" value="" /></td>
+		</tr>
+<?endif?>
+<?if ($arResult["REMEMBER_OTP"] == "Y"):?>
+		<tr>
+			<td valign="top"><input type="checkbox" id="OTP_REMEMBER_frm" name="OTP_REMEMBER" value="Y" /></td>
+			<td width="100%"><label for="OTP_REMEMBER_frm" title="<?echo GetMessage("auth_form_comp_otp_remember_title")?>"><?echo GetMessage("auth_form_comp_otp_remember")?></label></td>
+		</tr>
+<?endif?>
+		<tr>
+			<td colspan="2"><input type="submit" name="Login" value="<?=GetMessage("AUTH_LOGIN_BUTTON")?>" /></td>
+		</tr>
+		<tr>
+			<td colspan="2">
+				<noindex>	
+				<a href="<?=$arResult["AUTH_LOGIN_URL"]?>" rel="nofollow"><?echo GetMessage("auth_form_comp_auth")?></a></noindex><br /></td>
+		</tr>
+	</table>
+</form>
+
+<?
+else:
+?>
+
+<form action="<?=$arResult["AUTH_URL"]?>">
+	<table width="95%">
+		<tr>
+			<td align="center">
+				<?=$arResult["USER_NAME"]?><br />
+				[<?=$arResult["USER_LOGIN"]?>]<br />
+				<a href="<?=$arResult["PROFILE_URL"]?>" title="<?=GetMessage("AUTH_PROFILE")?>"><?=GetMessage("AUTH_PROFILE")?></a><br />
+			</td>
+		</tr>
+		<tr>
+			<td align="center">
+			<?foreach ($arResult["GET"] as $key => $value):?>
+				<input type="hidden" name="<?=$key?>" value="<?=$value?>" />
+			<?endforeach?>
+			<input type="hidden" name="logout" value="yes" />
+			<input type="submit" name="logout_butt" value="<?=GetMessage("AUTH_LOGOUT_BUTTON")?>" />
+			</td>
+		</tr>
+	</table>
+</form>
+<?endif?>
+</div>
